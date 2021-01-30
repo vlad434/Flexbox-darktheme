4e73153953f3044bc0bc68c9c6b70552e61f3431
unknown <vladilie06@yahoo.com>
2021-01-30 12:31:15 +0200

    first

---
+++ script.js
@@ -0,0 +1,30 @@
+document.getElementById("theme-1-btn").addEventListener("click", () => {
+  document.documentElement.style.setProperty("--background-color", "red");
+});
+
+document.getElementById("theme-2-btn").addEventListener("click", () => {
+  document.documentElement.style.setProperty("--background-color", "yellow");
+});
+
+
+
+document.getElementById("dark-theme-btn").addEventListener("click", () => {
+  document.documentElement.style.setProperty("--background-color", "#333");
+});
+
+
+document.getElementById("light-theme-btn").addEventListener("click", () => {
+  document.documentElement.style.setProperty("--background-color", "white");
+});
+
+
+
+
+
+document.getElementById("theme-3-btn").addEventListener("click", () => {
+  document.documentElement.style.setProperty("--background-color", "blue");
+});
+
+document.getElementById("theme-4-btn").addEventListener("click", () => {
+  document.documentElement.style.setProperty("--background-color", "cyan");
+});
